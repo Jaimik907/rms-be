84669bd39d7aae6374b22b99b8906419b60a9dab
Shubham Rathod <rathodshubham063@gmail.com>
2023-04-02 21:04:48 +0530

    Add new rules for prettierrc and add files for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common'
+import {AppController} from './app.controller'
+import {AppService} from './app.service'
+import {ConfigModule} from '@nestjs/config'
 
 @Module({
-  imports: [],
+  // to load the .env file include ConfigModule
+  imports: [ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
